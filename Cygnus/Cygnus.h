6c24030748d6c94c86d236543ea3d977299df5d1
mikehobi <mikehobizal@gmail.com>
2019-06-24 13:36:25 -0700

    Refactor

---
+++ Cygnus/Cygnus.h
@@ -0,0 +1,19 @@
+//
+//  Cygnuss.h
+//  Cygnuss
+//
+//  Created by Mike Hobizal on 6/18/19.
+//  Copyright © 2019 Mike Hobizal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Cygnuss.
+FOUNDATION_EXPORT double CygnussVersionNumber;
+
+//! Project version string for Cygnuss.
+FOUNDATION_EXPORT const unsigned char CygnussVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Cygnuss/PublicHeader.h>
+
+
