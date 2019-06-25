8544af258d9b0da544ee1dc057c1fc4db1548295
mikehobi <mikehobizal@gmail.com>
2019-06-24 17:18:27 -0700

    Add example project

---
+++ Example-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example-iOS
+//
+//  Created by Mike Hobizal on 6/24/19.
+//  Copyright © 2019 Mike Hobizal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
