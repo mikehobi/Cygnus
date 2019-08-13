5d9c185342b2faf4a5aa85c543baa76009ecb2d1
Mike Hobizal <mikehobizal@gmail.com>
2019-08-13 09:55:24 -0700

    Test earlier bonmot version

---
+++ Cygnus/Components/Test.swift
@@ -0,0 +1,15 @@
+//
+//  Test.swift
+//  Cygnus
+//
+//  Created by Mike Hobizal on 8/12/19.
+//  Copyright © 2019 Mike Hobizal. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import BonMot
+import SnapKit
+
+public class Test: UIView {
+}
